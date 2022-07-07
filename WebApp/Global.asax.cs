d4dd67b9ebed3e492a19c6333d4df61ca41eaf0a
Madhukar Moogala <madhukar.moogala@autodesk.com>
2022-07-07 15:42:51 +0530

    cleaning global.asax

--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -14,48 +14,10 @@ namespace WebApp
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        //class FakeHost : HostApplicationServices
-        //{
-        //    public override string FindFile(string fileName, Database database, FindFileHint hint)
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
-        //static void PreLoad(string strRealDwgPath)
-        //{
-        //    //
-        //    // this needs to be outside so I can find acdb23.dll separately from AcDbMgd
 
-        //    //make sure we find our unmanaged dependencies (add folder to the %PATH%)
-        //    var path = Environment.GetEnvironmentVariable("PATH");
-        //    Environment.SetEnvironmentVariable("PATH", $"{strRealDwgPath};{path}");
-
-        //    AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-        //    {
-        //        // we get here if assembly resolution fails, let's see if it was acdbmgd
-        //        if (args.Name.StartsWith("Acdbmgd,"))
-        //        {
-        //            //load acdbmgd from the requested folder
-        //            return Assembly.LoadFrom(Path.Combine(strRealDwgPath, "acdbmgd.dll"));
-        //        }
-        //        return null;
-        //    };
-        //}
-
-        //[MethodImpl(MethodImplOptions.NoInlining)]
-        //static void LoadAcDbMgd()
-        //{
-
-        //    Autodesk.AutoCAD.Runtime.RuntimeSystem.Initialize(new FakeHost(), 0x409);
-        //}
-        //static void LoadRealDwgFrom(string folder)
-        //{
-        //    PreLoad(folder);
-        //    LoadAcDbMgd();
-        //}
         protected void Application_Start()
         {
-            //LoadRealDwgFrom(@"D:\Rd23\RealDWG 2023\");
+            
             GlobalConfiguration.Configure(WebApiConfig.Register);
             
         }
